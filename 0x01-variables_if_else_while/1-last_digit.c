006c77d88b9698fe966189e6e1e24a287b5dda9d
Solomon <ezeatumsolomon@gmail.com>
2022-07-01 20:20:18 +0000

    First commit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *  main - print lowercase a-z
+ *  Return: Always 0 Unles error occurs
+ */
+
+int main(void)
+{
+	char alpha = 'a';
+	while (alpha <= 'z')
+	{
+	putchar(alpha);
+	alpha++;
+	putchar('\n');
+	return (0);
+	}
+}
