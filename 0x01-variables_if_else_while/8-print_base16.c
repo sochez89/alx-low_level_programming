64512be864765a8ebc9f86aa38601e9e1e2f6dde
Solomon <ezeatumsolomon@gmail.com>
2022-07-03 04:13:47 +0000

    First commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print all the numbers of base 16 in lower case
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int num = 0;
+	char num_10_16 = 'a';
+
+	while (num < 9)
+	{
+		putchar(num);
+		num++;
+	}
+	while (num_10_16 < 'g')
+	{
+		putchar(num_10_16);
+		num_10_16++;
+	}
+	putchar('\n');
+return (0);
+}
