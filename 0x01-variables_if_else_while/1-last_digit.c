25d17faba924c11503a72aedc4ff466c3a5768ab
Solomon <ezeatumsolomon@gmail.com>
2022-07-04 18:40:28 +0000

    First commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,19 +1,28 @@
 #include <stdio.h>
-
+#include <time.h>
+#include <stdlib.h>
 /**
- *  main - print lowercase a-z
+ *  main - print random numbers
  *  Return: Always 0 Unles error occurs
  */
 
 int main(void)
 {
-	char alpha = "a";
+	int n;
 
-	while (alpha <= "z")
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 5)
 	{
-	putchar(alpha);
-	alpha++;
-	putchar("\n");
-	return (0);
+		printf("Last digit of %i is  greater than\n 5", n);
 	}
+	else if (n == 0)
+	{
+		printf("Last digit of %i and is 0\n", n);
+	}
+	else if (n < 6 && n > 0)
+	{
+		printf("Last digit of %i and is les than 6 and not 0\n", n);
+	}
+	return (0);
 }
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible combinations of single digit numbers
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i, j;
+
+	i = '0';
+	j = '0';
+
+	for (i <= '9';)
+	{
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+		i++;
+		for (j <= 9;)
+		{
+			putchar(ij);
+			putchar(',');
+			putchar(' ');
+			ij++
+
+		}
+		putchar('\n');
+	}
+}
