b305deb96fb3b77125920a195f49dc5ab8faa90b
Solomon <ezeatumsolomon@gmail.com>
2022-07-04 19:45:54 +0000

    quick fix

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,17 +7,10 @@
 
 int main(void)
 {
-<<<<<<< HEAD
 	int num = '0';
 	char num_10_16 = 'a';
 
-	while (num <= '9')
-=======
-	int num = 0;
-	char num_10_16 = 'a';
-
 	while (num < 9)
->>>>>>> 64512be864765a8ebc9f86aa38601e9e1e2f6dde
 	{
 		putchar(num);
 		num++;
