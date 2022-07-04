2be0ec1519afd0e93d8717768abe3694b8514cf0
Solomon <ezeatumsolomon@gmail.com>
2022-07-04 19:41:34 +0000

    correct conditional statement

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ int main(void)
 	{
 		printf("Last digit of %i and is 0\n", n);
 	}
-	else if (6 < (n % 10) != 0)
+	else if ((n % 10) < 6 && (n % 10) != 0)
 	{
 		printf("Last digit of %i and is les than 6 and not 0\n", n);
 	}
