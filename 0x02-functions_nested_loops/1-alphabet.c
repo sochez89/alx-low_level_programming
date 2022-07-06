759fb1aee8ca9166db8cf3937a9e70ff5c616b01
Solomon <ezeatumsolomon@gmail.com>
2022-07-06 20:18:46 +0000

    First commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,10 +9,9 @@ void print_alphabet(void)
 {
 	char alpha = 'a';
 
-	for (alpha == 'a'; alpha <= 'z'; alpha++)
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
 	{
 		_putchar(alpha);
 	}
 	_putchar('\n');
-	return (0);
 }
---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/** 
+ * main - print all other functions
+ * Return: 0
+ */
+
+int main(void)
+{
+	void print_alphabet(void);
+	return (0);
+}
