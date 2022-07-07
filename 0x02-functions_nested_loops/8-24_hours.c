b3c4d32ff208fdad62832b4b6229ac2aa4eaeba1
Solomon <ezeatumsolomon@gmail.com>
2022-07-07 01:08:38 +0000

    becoming interesting

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * jack_bauer: print every minute of the day starting from 00:00 to 23:59.
+ * Return: 0 shows the program ruan succesfully
+ */
+
+void jack_bauer(void)
+{
+	int H, M, h, m;
+
+	for (H = 0; H <= 2; H++)
+	{
+		for (h = 0; h <= 9; h++)
+		{
+			for (M = 0; M < 6; M++)
+			{
+				for (m = 0; m <= 9; m++)
+				{
+					if (!((H == 2) && (h > 3)))
+					{
+						_putchar('0' + H);
+						_putchar('0' + h);
+						_putchar(':');
+						_putchar('0' + M);
+						_putchar('0' + m);
+						_putchar('\n');
+					}
+				}
+			}
+		}
+	}
+}
