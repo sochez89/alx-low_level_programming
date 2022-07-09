b54fa3d680ad3f6aa84067f9e48e29dbbda813e8
Solomon <ezeatumsolomon@gmail.com>
2022-07-09 15:25:04 +0000

    print numbers 0-9 except 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,13 +9,12 @@ void print_most_numbers(void)
 {
 	int n;
 
-	for (n = 0; n <= 9;)
+	for (n = '0'; n <= '9'; n++)
 	{
-		if (n != 2 && n != 4)
+		if (n != '2' && n != '4')
 		{
 			_putchar(n);
 		}
-		n++;
 	}
 	_putchar('\n');
 }
