d19d42d2a08140b9e50dae4c5c12e3bb2569467e
Solomon <ezeatumsolomon@gmail.com>
2022-07-10 13:20:10 +0000

    quick commit

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,21 +7,30 @@
 
 void print_diagonal(int n)
 {
-	int i, j
-		;
 	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
+		int i;
+
 		for (i = 1; i <= n; i++)
 		{
+			int j;
+
 			for (j = 1; j <= i; j++)
 			{
+				if (j == i)
+				{
+					_putchar('\\');
+				}
+				else
+				{
+					_putchar(' ');
+				}
 				_putchar(' ');
 			}
-			_putchar('\\');
 			_putchar('\n');
 		}
 	}
