35d45c91aeb2515ebfdde60ff79be8fe162cc96b
Solomon <ezeatumsolomon@gmail.com>
2022-07-11 16:16:45 +0000

    corrcet number of space

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,6 +11,7 @@ int main(void)
 
 	printf("Infinite loop incoming :(\n");
 	i = 0;
+	
 	/**
 	 * while (i < 10)
 	 * {
@@ -19,6 +20,5 @@ int main(void)
 	 */
 	
 	printf("Infinite loop avoided! \\o/\n");
-	
 	return (0);
 }
