7733fe73479d12f550c836b6bacb5a13652097a7
Solomon <ezeatumsolomon@gmail.com>
2022-07-14 19:40:17 +0000

    correct simple look error

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ * context: prints the number 1 - 100
+ * Fizz for multiples 0f 3, Buzz for multiples of 5 and
+ * FizzBuzz for multiples of both
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 100; a++)
+	{
+		if (a % 3 == 0)
+			printf("Fizz");
+		else if (a % 5 == 0)
+			printf("Buzz");
+		else if (a % 3 == 0)
+			printf("FizzBuzz");
+		else
+			printf("%i", a);
+		if (i < 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
