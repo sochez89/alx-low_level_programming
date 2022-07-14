5f41b680b4f2e75b4dee07efb607c352f12b4cb4
Solomon <ezeatumsolomon@gmail.com>
2022-07-14 20:06:44 +0000

    correct iteration error

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,12 +15,12 @@ int main(void)
 
 	for (a = 0; a < 100; a++)
 	{
-		if (a % 3 == 0)
+		if (a % 15 == 0)
+			printf("FizzBuzz");
+		else if (a % 3 == 0)
 			printf("Fizz");
 		else if (a % 5 == 0)
 			printf("Buzz");
-		else if (a % 3 == 0)
-			printf("FizzBuzz");
 		else
 			printf("%i", a);
 		if (a < 100)
