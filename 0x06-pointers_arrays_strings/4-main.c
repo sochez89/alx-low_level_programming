2e2fd77ddf17d784cc9eb7e2cd0396306309a2fa
Solomon <ezeatumsolomon@gmail.com>
2022-07-17 15:52:41 +0000

    hot fix

---
+++ 0x06-pointers_arrays_strings/4-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+int main(void)
+{
+	void reverse_array();
+    int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
+
+    print_array(a, sizeof(a) / sizeof(int));
+    reverse_array(a, sizeof(a) / sizeof(int));
+    print_array(a, sizeof(a) / sizeof(int));
+    return (0);
+}
