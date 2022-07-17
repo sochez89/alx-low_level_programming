0d62c1459ab6fca9afb5e2565c2238a95f4de4cb
Solomon <ezeatumsolomon@gmail.com>
2022-07-17 13:08:06 +0000

    correct loop error

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -20,10 +20,10 @@ char *_strncpy(char *dest, char *src, int n)
 	for (index = 0; src[index] && index < n; index++)
 	{
 		dest[index] = src[index];
-		for (index = src_len; index < n; index++)
-		{
-			dest[index] = '\0';
-		}
+	}
+	for (index = src_len; index < n; index++)
+	{
+		dest[index] = '\0';
 	}
 	return (dest);
 
