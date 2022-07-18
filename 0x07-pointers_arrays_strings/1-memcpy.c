7a078e8d5d08b2914377a73c357f44a1e0904f75
Solomon <ezeatumsolomon@gmail.com>
2022-07-18 11:13:46 +0000

    first commit

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *_memcpy - copy n bytes from one memory location to another
+ * @dest: memory destination
+ * @src: memory source (where to copy from)
+ * @n: integer value representing the bumber of bytes to be copied
+ * Return: returns a pointer to memory location named dest
+ */
+
+char *_memcpy(char *dest, char *src, insigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+
+	}
+}
