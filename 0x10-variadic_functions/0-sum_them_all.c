8c38df96400c46674bc2441695c9a19c9f441bed
Solomon <ezeatumsolomon@gmail.com>
2022-08-12 10:51:59 +0000

    header functions

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - function that returns sum of all its parameters
+ * @n: represents the number of parameters of the function above
+ * Return: returns 0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int i, sum;
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,3 @@
+What are variadic functions
+How to use va_start, va_arg and va_end macros
+why and how to use the const type qualifier
